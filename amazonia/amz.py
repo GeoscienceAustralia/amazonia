a3a6d47117bd86686347ffe4336160d5e1a2835d
Fahad Talib <fuddstar@gmail.com>
2016-06-01 11:57:16 +1000

    AUTO-510 changing amz to relative paths

--- amazonia/amz.py
+++ amazonia/amz.py
@@ -4,6 +4,7 @@
 Ingest User YAML and defaults YAML and send to yaml class to return as one unified data dictionary for stack input
 
 """
+import os
 import yaml
 import argparse
 from amazonia.classes.yaml import Yaml
@@ -40,19 +41,19 @@ def main():
     Create Stack template from stack output
     """
 
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-y', '--yaml',
-                        default='./application.yaml',
+                        default=os.path.join(__location__, 'application.yaml'),
                         help="Path to the applications amazonia yaml file")
     parser.add_argument('-d', '--default',
-                        default='./defaults.yaml',
-                        help="Path to the environmental defaults yaml file")
-    parser.add_argument('-s', '--schema',
-                        default='./schema.yaml',
-                        help="Path to the schema to validate the provided yaml values against")
+                        default=os.path.join(__location__, './defaults.yaml'),
+                        help='Path to the environmental defaults yaml file')
     parser.add_argument('-t', '--template',
                         default='stack.template',
-                        help="Path for amazonia to place template file")
+                        help='Path for amazonia to place template file')
     args = parser.parse_args()
 
     """ YAML ingestion
