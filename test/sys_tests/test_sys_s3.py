b47898d7849335a0ffc28e9a3239e602dafb153c
Fahad Talib <fuddstar@gmail.com>
2016-06-22 12:39:39 +1000

    Adding docstring for main in test_sys_s3

--- test/sys_tests/test_sys_s3.py
+++ test/sys_tests/test_sys_s3.py
@@ -4,6 +4,9 @@ from amazonia.classes.s3 import S3
 
 
 def main():
+    """
+    Creates a troposphere template and then adds a single s3 bucket
+    """
     template = Template()
 
     S3(unit_title='MyBucket',
