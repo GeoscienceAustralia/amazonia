abf42f6079b4b6198b9e10bfe909fc700040eae5
Fahad Talib <fuddstar@gmail.com>
2016-07-08 09:26:51 +1000

    changing acc number for policy testing

--- amazonia/classes/cloudtrail.py
+++ amazonia/classes/cloudtrail.py
@@ -15,7 +15,7 @@ class Cloudtrail(object):
         title = unit_title + 'Trail'
 
         policy = {
-            "Version": "2012-10-17",
+            # "Version": "2012-10-17",
             "Statement": [
                 {
                     "Effect": "Allow",
@@ -27,7 +27,7 @@ class Cloudtrail(object):
                     "Effect": "Allow",
                     "Principal": {"Service": "cloudtrail.amazonaws.com"},
                     "Action": "s3:PutObject",
-                    "Resource": "arn:aws:s3:::mytrailamzs3/AWSLogs/123456789123/*",
+                    "Resource": "arn:aws:s3:::mytrailamzs3/AWSLogs/123456789/*",
                     "Condition": {
                         "StringEquals": {
                             "s3:x-amz-acl": "bucket-owner-full-control"
