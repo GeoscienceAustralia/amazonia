132780ec80a0b8a2ce2c6b65d0e10c2fb1a0d1d2
Fahad Talib <fuddstar@gmail.com>
2016-07-08 19:01:05 +1000

    finishing cloudtrail unit tests

--- test/unit_tests/test_cloudtrail.py
+++ test/unit_tests/test_cloudtrail.py
@@ -15,20 +15,30 @@ def test_s3_title():
     test_s3_names = ['MyTest', 'Tests3', 'Supers3bucket']
 
     for name in test_s3_names:
-        cloud_trail = (Cloudtrail(name, template)
-        assert_equals(cloud_trail.s3_b.title, (name + 'amzs3').lower())
+        cloud_trail = (Cloudtrail(name, template))
+        assert_equals(cloud_trail.s3_b_trail.s3_b.title, (name + 'trail' + 'amzs3').lower())
 
 
 def test_s3_bucket_policy():
     """
     To test that bucket policy is applied is passed in
     """
+    template = Template()
+
+    cloud_trail = (Cloudtrail('MyTrail', template))
+
+    # Test policy matches
+    assert_equals(type(cloud_trail.s3_b_trail.s3_b_policy.PolicyDocument), dict)
+
+    # Test bucket name is policy matches
+    assert_equals(cloud_trail.s3_b_trail.s3_b_policy.Bucket, 'mytrailtrailamzs3')
 
 
 def test_s3_bucket_name_in_cloudtrail():
     """
     To test the s3 bucket name in cloudtrail
     """
+    template = Template()
 
-
-def test_cloudtrail_depends_on_s3_bucket_policy()
+    cloud_trail = (Cloudtrail('MyTrail', template))
+    assert_equals(cloud_trail.trail.S3BucketName, 'mytrailtrailamzs3')
