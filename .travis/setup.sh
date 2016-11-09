ce933f8f006eeabc1f729fdfaccfe839e2ae5d38
James Kingsmill <james.kingsmill@gmail.com>
2016-11-09 12:57:36 +1100

    Move travis before_install to separate file

---
+++ .travis/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y pylint
+curl http://repo1.maven.org/maven2/org/codehaus/sonar/runner/sonar-runner-dist/2.4/sonar-runner-dist-2.4.zip -o /tmp/sonar-runner-dist-2.4.zip
+sudo mkdir /opt/sonar-runner
+sudo unzip /tmp/sonar-runner-dist-2.4.zip -d /opt/sonar-runner
+sudo chmod 755 -R /opt/sonar-runner
+export PATH=$PATH:/opt/sonar-runner/sonar-runner-2.4/bin
