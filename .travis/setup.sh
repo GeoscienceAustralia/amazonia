3f4e91e8645d891622c2262846d5847c2d9753bd
James Kingsmill <james.kingsmill@gmail.com>
2016-11-10 15:12:36 +1100

    Use dodo branch to test removal of cloudfront

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -7,4 +7,5 @@ sudo mkdir /opt/sonar-runner
 sudo unzip /tmp/sonar-runner-dist-2.4.zip -d /opt/sonar-runner
 sudo chmod 755 -R /opt/sonar-runner
 export PATH=$PATH:/opt/sonar-runner/sonar-runner-2.4/bin
-git clone -b integration $INFRA_REPO
+#git clone -b integration $INFRA_REPO
+git clone -b dev-aynsof-systestseparation-AUTO-895 $INFRA_REPO
