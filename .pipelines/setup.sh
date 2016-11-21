94b3f824c5405c59d16271b38b3173340044f7b2
James Kingsmill <james.kingsmill@gmail.com>
2016-11-21 15:42:21 +1100

    Add pipelines scripts and yaml

---
+++ .pipelines/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+git config --global push.default simple
+git clone -b integration $INFRA_REPO
---
+++ .pipelines/systems-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip3 install -e . --upgrade
+git clone -b integration $INFRA_REPO
+
+#python3 dodo/test/sys_tests/test_sys_stack_create.py -y amazonia/application.yaml -d dodo/configuration_code/scripts/amazonia/amazonia/amazonia_ga_defaults.yaml -t testSysStackGA.template -st testSysStackGA &
+#python3 dodo/test/sys_tests/test_sys_stack_create.py -y amazonia/application.yaml -d amazonia/defaults.yaml -s amazonia/schema.yaml -t testSysStack.template -st testSysStack &
+#python3 dodo/test/sys_tests/test_sys_stack_create.py -y dodo/configuration_code/scripts/amazonia/amazonia/amazonia_verbose_example.yaml -d amazonia/defaults.yaml -t testSysStackVerbose.template -st testSysStackVerbose -p '[{"ParameterKey":"db1MasterPassword","ParameterValue":"password123","UsePreviousValue":false},{"ParameterKey":"db1MasterUsername","ParameterValue":"testdbusername","UsePreviousValue":false}]'
