c5e45c698e834853307c104af2a21df4cf0e0f68
Ga <ga.autobots@gmail.com>
2016-11-23 03:54:50 +0000

    Bump version: 1.4.23 → 1.4.24

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='amazonia',
-    version='1.4.23',
+    version='1.4.24',
     description="GA AWS CloudFormation creation library",
     author="The Geoscience Australia Autobots, and Lazar Bodor",
     author_email="autobots@ga.gov.au , lazar.bodor@ga.gov.au",
